48f8b6aaa81a65665852f9ee1da8197eb6bdca2d
anonymous0909-cpu <mokshsharma9008.business@gmail.com>
2025-09-26 11:53:35 +0530

    first commit

---
+++ showsidebar.js
@@ -0,0 +1,9 @@
+function ShowSideBar() {
+    const sideBar = document.querySelector('.side-bar')
+    sideBar.style.display = 'flex'
+}
+
+function HideSideBar() {
+     const sideBar = document.querySelector('.side-bar')
+    sideBar.style.display = 'none'
+}
